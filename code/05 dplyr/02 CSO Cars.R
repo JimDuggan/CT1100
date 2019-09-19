f6f52fd5b001eabe870dcde8adca1dfe553664af
Jim Duggan <jim@MyMacBook.local>
2019-09-19 11:01:51 +0100

    Adding comments & dplyr call

--- code/05 dplyr/02 CSO Cars.R
+++ code/05 dplyr/02 CSO Cars.R
@@ -2,10 +2,16 @@ library(dplyr)
 library(ggplot2)
 source("code/05 dplyr/TidyCSOCars.R")
 
+# get the data in tidy format
 cso_cars <- tidy_cso()
 
+# plot the area graph
 ggplot(data=cso_cars,
        mapping=aes(x=Year,y=NewPrivateCars,colour=CarType,fill=CarType)) +
   geom_line()+geom_area()+ggtitle("New Private Cars 1998-2018")+
   ylab("Number of Registrations")
+
+# calculate the sum for each year
+cso_cars %>% group_by(Year) %>% 
+             summarise(Total=sum(NewPrivateCars))
 
--- code/05 dplyr/TidyCSOCars.R
+++ code/05 dplyr/TidyCSOCars.R
@@ -1,6 +1,8 @@
 library(tidyr)
 library(readxl)
 
+# convert the data to tidy data format, where each 
+# row is an observation, and each column a variable
 tidy_cso <- function (){
   cso_wide <- read_excel("datasets/economic/CarsIreland.xlsx")
   cso_wide %>% filter(CarType!= "All fuel types") %>%
