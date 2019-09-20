7dd45ee1f81666579527f145a94c5b615ce4f933
Jim Duggan <jim@MyMacBook.local>
2019-09-20 22:36:52 +0100

    Adding package summary

--- code/06 stocks/01 Stock Prices.R
+++ code/06 stocks/01 Stock Prices.R
@@ -5,6 +5,11 @@ prices <- getSymbols(c("AMZN","AAPL","^DJI"),
                      from="2019-01-01",
                      to="2019-09-19")
 
+apple <- getSymbols(c("AAPL"),
+                     auto.assign = T,
+                     from="1990-01-01",
+                     to="2019-09-19")
+
 chartSeries(AMZN,theme="white")
 chartSeries(DJI,theme="white")
 chartSeries(AAPL,theme="white")
