5b6707aaed75d1772e86c6df6ebd907b63edb8db
Jim Duggan <jim@MyMacBook.local>
2019-09-25 22:37:18 +0100

    Updating lectures

--- code/05 dplyr/03 All Irelands.R
+++ code/05 dplyr/03 All Irelands.R
@@ -6,7 +6,7 @@ data <- readxl::read_excel("datasets/sport/AllIrelandsResults.xlsx")
 data %>% group_by(Winner) %>% summarise(Wins=n()) %>%
          arrange(desc(Wins))
 
-data %>% group_by(Runner-up) %>% summarise(Losses=n()) %>%
-  arrange(desc(Wins))
+data %>% group_by(RunnerUp) %>% summarise(Losses=n()) %>%
+  arrange(desc(Losses))
 
                                           
